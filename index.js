3a85a0f2124494a69988732ebfc27b96200f547f
ksvvr <ksvvreddy@gmail.com>
2024-01-17 23:05:33 +0530

    website index page made visible initially

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const lms = require("./lms");
+const lms = require('./lms')
 
 lms.listen(4000, () => {
-  console.log("Started express server at port 4000");
-});
+  console.log('Started express server at port 4000')
+})
--- lms.js
+++ lms.js
@@ -1,5 +1,28 @@
+/* eslint-disable no-unused-vars */
 const express = require('express')
-// var csrf = require('tiny-csrf')
+const csrf = require('tiny-csrf')
 const lms = express()
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+const path = require('path')
+const passport = require('passport')
+const connectEnsureLogin = require('connect-ensure-login')
+const session = require('express-session')
+const flash = require('connect-flash')
+lms.set('views', path.join(__dirname, 'views'))
+const LocalStrategy = require('passport-local')
+const bcrypt = require('bcrypt')
+
+lms.use(flash())
+lms.use(bodyParser.json())
+lms.use(express.urlencoded({ extended: false }))
+lms.use(cookieParser('shh!some secret string,I am King'))
+lms.use(csrf('abcdefghijklmnopqrstuvwxyz123456', ['POST', 'PUT', 'DELETE']))
+lms.set('view engine', 'ejs')
+lms.use(express.static(path.join(__dirname, 'public')))
+
+lms.get('/', async (request, response) => {
+  response.render('index.ejs')
+})
 
 module.exports = lms
