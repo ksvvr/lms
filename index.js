5881ec3b5f14a2dcc178dfede51025209ad202b0
ksvvr <ksvvreddy@gmail.com>
2024-01-17 19:40:53 +0530

    created views folder; initiated sequelize-cli config model;

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-console.log('Hello World!')
+const lms = require("./lms");
+
+lms.listen(4000, () => {
+  console.log("Started express server at port 4000");
+});
---
+++ lms.js
@@ -0,0 +1,5 @@
+const express = require("express");
+// var csrf = require('tiny-csrf')
+const lms = express();
+
+module.exports = lms;
